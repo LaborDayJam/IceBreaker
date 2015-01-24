f0e8d15bb6c563364b19f1a06954f48679b3f05b
gamingod4u <mcman407@gmail.com>
2015-01-24 09:13:29 -0500

    Created level generator

---
+++ Assets/Scripts/IceCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceCube : MonoBehaviour 
+{
+
+	private GameObject iceContainer;
+	// Use this for initialization
+	void Awake () 
+	{
+		this.transform.parent = GameObject.FindGameObjectWithTag("Container").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour 
+{
+	public GameObject iceblock;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		LevelGenerator();
+	}
+
+	void LevelGenerator()
+	{
+		for(int y = 0; y < 10; y++)
+			for(int x = 0; x < 20; x++)
+				for(int z = 0; z < 20; z++)
+					 Instantiate(iceblock, new Vector3(x,y,z),Quaternion.identity);
+
+			
+	}
+}

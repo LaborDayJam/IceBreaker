d0d512d08512ed811cdf68a6e679abb1163f81e3
John Li <johnliglobal@gmail.com>
2015-01-24 17:33:25 -0500

    Added combat game mode

--- Assets/Scripts/GameModeCollect.cs
+++ Assets/Scripts/GameModeCollect.cs
@@ -4,11 +4,6 @@ using System.Collections.Generic;
 
 public class GameModeCollect : GameMode {
 
-	//Team Properties
-	public const int TEAM_IGLOO_INDEX = 0;
-	public const int TEAM_ICEBREAKER_INDEX = 1;
-	List<Player> teamIgloo;
-	List<Player> teamIcebreaker;
 
 	//Time Limit
 	float timeLimit = 120; //seconds
@@ -17,15 +12,8 @@ public class GameModeCollect : GameMode {
 	//Point Goal
 	int winningScoreGoal = 10;
 
-	//Map 
-	public Level level;
-
-	//Prefab
-	public GameObject prefabPlayer;
-	public GameObject prefabDummyPlayer; //for local player two
-
 	private static GameModeCollect instance;
-	public GameModeCollect Instance { get { return instance; } }
+	public static  GameModeCollect Instance { get { return instance; } }
 
 	void Awake()
 	{
@@ -37,50 +25,7 @@ public class GameModeCollect : GameMode {
 		}
 	}
 
-	void Start () 
-	{
-		teamIgloo = new List<Player> ();
-		teamIcebreaker = new List<Player> ();
-		StartGame ();
-	}
 
-	void StartGame()
-	{
-		timeLeft = timeLimit;
-		SpawnPlayers ();
-		StartCoroutine (CR_GameLogicLoop ());
-		StartCoroutine (CR_CheckWinCondition ());
-		StartCoroutine (CR_CheckLoseCondition ());
-	}
-
-	void SpawnPlayers()
-	{
-		GameObject playerOne = Instantiate (prefabPlayer, new Vector3 (0, 18, 0), Quaternion.identity) as GameObject;
-		addPlayer (0, playerOne.GetComponent<Player>());
-
-		//Dont instantiate dummy player if 2 player local is supported
-		// use regular player
-		GameObject playerTwo = Instantiate (prefabDummyPlayer, new Vector3 (0, 18, 0), Quaternion.identity) as GameObject;
-		addPlayer (1, playerTwo.GetComponent<Player>());
-	}
-
-	void addPlayer(int team, Player player)
-	{
-		if (team == TEAM_IGLOO_INDEX) {
-			teamIgloo.Add(player);
-		} else {
-			teamIcebreaker.Add(player);
-		}
-	}
-
-	void removePlayer(int team, Player player)
-	{
-		if (team == TEAM_IGLOO_INDEX) {
-			teamIgloo.Remove(player);
-		} else {
-			teamIcebreaker.Remove(player);
-		}
-	}
 
 	protected override void GameOver()
 	{
---
+++ Assets/Scripts/GameModeCombat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModeCombat : GameMode {
+	
+	private static GameModeCombat instance;
+	public static GameModeCombat Instance { get { return instance; } }
+	
+	void Awake()
+	{
+		if(instance == null)
+			instance = this;
+		else{
+			Debug.Log("WARNING: DUPLICATE GAME MODE OBJECT. DESTROYING SELF");
+			Destroy(gameObject);
+		}
+	}
+
+	public void PlayerFell(Player player)
+	{
+		GameOver();
+		
+		if (player.team == TEAM_ICEBREAKER_INDEX) {
+			Debug.Log ("Team Igloo Wins"); 
+		} else
+			Debug.Log ("Team Icebreaker Wins");
+	}
+	
+	protected override IEnumerator CR_GameLogicLoop()
+	{
+		while (true) {
+			yield return new WaitForSeconds(3.0f); //check every 3 seconds
+		}
+	}
+}

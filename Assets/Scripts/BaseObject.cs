bca549e15dd1551a339c34714f0cd2dcebec1737
John Li <johnliglobal@gmail.com>
2015-01-24 11:35:57 -0500

    Added First Person Controller Added Breaking Ice Updated Map Generation

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseObject : MonoBehaviour {
+	public bool isTargetable;
+	public float health;
+
+	public virtual void onHit(GameObject other, float damage)
+	{
+		health -= damage;
+		if (health <= 0) {
+			Debug.Log("I shoudl be destroyed");
+		
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Player : BaseObject {
+
+	List<Action> actions;
+
+	public Action currentAction;
+
+	void Awake()
+	{
+		actions = new List<Action> ();
+		currentAction = gameObject.AddComponent<HitAction>();
+		actions.Add (currentAction);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch(currentAction.type)
+		{
+		case ACTION_TYPE.ONCE:
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				currentAction.Do();
+			}
+		}break;
+		case ACTION_TYPE.CONTINOUS:
+		{
+			if(Input.GetMouseButton(0))
+				currentAction.Do();
+			
+		}break;
+		default:
+
+			break;
+		}
+	}
+
+}

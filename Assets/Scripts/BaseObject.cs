c8f85e20b589ed54f2bc4b54e06e86faee4f352c
gamingod4u <mcman407@gmail.com>
2015-01-25 15:39:54 -0500

    added busted ice particle

--- Assets/Scripts/BaseObject.cs
+++ Assets/Scripts/BaseObject.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class BaseObject : MonoBehaviour {
 	public bool isTargetable;
 	public float health;
+	public GameObject icebreak;
 
 	public virtual void onHit(GameObject other, float damage)
 	{
+		
 		health -= damage;
 		if (health <= 0) {
+			Instantiate(icebreak, this.transform.position, Quaternion.identity);	
 			Destroy (gameObject);
 		}
 	}
